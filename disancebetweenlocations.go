9737007da89ce047b1387911ebd18e17fd60542b
Josh Montoya <josh@montoya.io>
2023-03-26 10:11:11 -0700

    implement distance between locations

---
+++ disancebetweenlocations.go
@@ -0,0 +1,14 @@
+package zipcodes
+
+type getDistanceBetweenLocationsResponse struct {
+	baseResponse
+
+	Result DistanceBetweenLocations `json:"result"`
+}
+
+type DistanceBetweenLocations struct {
+	FromZipCode string `json:"fromZipCode"`
+	ToZipCode   string `json:"toZipCode"`
+	Distance    string `json:"distance"`
+	Unit        string `json:"unit"`
+}
