cb8cf6f2198d2b1fc21ab44114cf7aae91e1088c
Josh Montoya <josh@montoya.io>
2023-03-26 10:10:46 -0700

    implement base response

---
+++ responses.go
@@ -0,0 +1,13 @@
+package zipcodes
+
+type apiResponse interface {
+	IsSuccessful() bool
+}
+
+type baseResponse struct {
+	Success bool `json:"success"`
+}
+
+func (b baseResponse) IsSuccessful() bool {
+	return b.Success
+}
